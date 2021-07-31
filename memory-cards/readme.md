95f32fdfadbac8d9c9e337235f05200b1fecf720
viet34tqc <viet34tqc@gmail.com>
2021-07-31 13:42:26 +0700

    Memory cards

---
+++ memory-cards/readme.md
@@ -0,0 +1,32 @@
+# Memory Cards
+
+## UI
+
+- Add new card
+  - A button to add new card
+  - A close button to close add-new page
+  - Add new card form: 2 inputs for question and answer
+- A button to clear all cards
+- A simple slider for cards
+
+## Interactions
+
+- Add new card
+  - When click add-new button, add-card page will be visible and override the current current page
+  - Click close button to back to the previouse page.
+  - Fill in the form and add card
+- Flip the card
+  - Click the card to flip it, then the answer will be revealed
+- Form
+  - Validation
+  - Successful submitting will close the form and add the card to the list.
+- Slider:
+  - Click next and back to jump between the cards
+- Clear the card:
+  - Clear the list of the cards then update the UI
+
+## What you will learn
+
+- Simple slider effect with navigation
+- Flip effect: explanation in the CSS code.
+- Basic form validation
---
+++ memory-cards/script.js
@@ -0,0 +1,150 @@
+(function () {
+    'use strict;';
+    // Select element
+    const formElem = document.getElementById('add-new-form');
+    const questionInput = document.getElementById('question');
+    const answerInput = document.getElementById('answer');
+    const triggerFormBtn = document.querySelector('.btn--trigger-form');
+    const closeFormBtn = document.querySelector('.btn--close-form');
+    const clearCardsBtn = document.querySelector('.btn--clear-cards');
+    const cardsSlider = document.querySelector('.cards-slider-wrapper');
+    const navPre = document.querySelector('.slider-prev');
+    const navNext = document.querySelector('.slider-next');
+    const cards = JSON.parse(localStorage.getItem('cards')) || [];
+    let activeIndex = 1;
+    // Validation form
+    function showError(input) {
+        const formGroup = input.parentElement;
+        const errorElem = formGroup.querySelector('.error');
+        const inputName = input.id.charAt(0).toUpperCase() + input.id.slice(1);
+        errorElem.innerText = `${inputName} is required`;
+    }
+    function checkRequiredForm(inputs) {
+        let isError = false;
+        inputs.forEach((input) => {
+            if (!input.value) {
+                isError = true;
+                showError(input);
+            }
+        });
+        return isError;
+    }
+    // Display form
+    triggerFormBtn.addEventListener('click', () => {
+        document.body.classList.add('is-visible-form');
+        questionInput.focus();
+    });
+    // Hide form
+    closeFormBtn.addEventListener('click', () => {
+        document.body.classList.remove('is-visible-form');
+    });
+    formElem.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const hasError = checkRequiredForm([questionInput, answerInput]);
+        if (hasError) {
+            return;
+        }
+        const question = questionInput.value;
+        const answer = answerInput.value;
+        questionInput.value = '';
+        answerInput.value = '';
+        addCard(question, answer);
+    });
+    function addCard(question, answer) {
+        const card = {
+            id: Math.random().toString(36).substr(2, 9),
+            question,
+            answer,
+        };
+        renderCard(card);
+        cards.push(card);
+        // Display the newest card.
+        const oldIndex = activeIndex;
+        activeIndex = cards.length;
+        moveSlider(oldIndex);
+        document.body.classList.remove('is-visible-form');
+        // Save to local storage
+        localStorage.setItem('cards', JSON.stringify(cards));
+    }
+    function renderCard(card) {
+        const activeClass = cards.length === 0 ? 'active' : '';
+        const cardHTML = `
+			<div class="card ${activeClass}">
+				<div class="card-inner">
+					<div class="card__answer">${card.answer}</div>
+					<div class="card__question">${card.question}</div>
+				</div>
+			</div>
+		`;
+        cardsSlider.insertAdjacentHTML('beforeend', cardHTML);
+    }
+    function renderCards() {
+        if (!cards.length) {
+            cardsSlider.innerHTML = '';
+        }
+        let cardsHTML = '';
+        cards.forEach((card, index) => {
+            cardsHTML += `
+				<div class="card ${index + 1 === activeIndex ? 'active' : ''}">
+					<div class="card-inner">
+						<div class="card__answer">${card.answer}</div>
+						<div class="card__question">${card.question}</div>
+					</div>
+				</div>
+			`;
+        });
+        cardsSlider.insertAdjacentHTML('beforeend', cardsHTML);
+    }
+    function clearCards() {
+        clearCardsBtn.addEventListener('click', () => {
+            cards.length = 0;
+            localStorage.setItem('cards', '[]');
+            renderCards();
+        });
+    }
+    function flipCard() {
+        document.addEventListener('click', (e) => {
+            const target = e.target;
+            if (!target.classList.contains('card__question') &&
+                !target.classList.contains('card__answer')) {
+                return;
+            }
+            let parentElem = target.parentElement;
+            while (!parentElem.classList.contains('card')) {
+                parentElem = parentElem.parentElement;
+            }
+            parentElem.classList.toggle('show-answer');
+        });
+    }
+    function handleSlider() {
+        navNext.addEventListener('click', () => {
+            const cardsLength = cards.length;
+            activeIndex++;
+            if (activeIndex > cardsLength) {
+                activeIndex = cardsLength;
+                return;
+            }
+            moveSlider(activeIndex - 1);
+        });
+        navPre.addEventListener('click', () => {
+            activeIndex--;
+            if (activeIndex < 1) {
+                activeIndex = 1;
+                return;
+            }
+            moveSlider(activeIndex + 1);
+        });
+    }
+    function moveSlider(oldIndex) {
+        if (oldIndex === 0) {
+            return;
+        }
+        document.querySelector(`.card:nth-child(${oldIndex})`).className = `card ${oldIndex > activeIndex ? 'right' : 'left'}`;
+        document.querySelector(`.card:nth-child(${activeIndex})`).className =
+            'card active';
+    }
+    renderCards();
+    clearCards();
+    flipCard();
+    handleSlider();
+})();
